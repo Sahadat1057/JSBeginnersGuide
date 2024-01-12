ceae7a35bdd54365c9f1c75b25dc1bbd0f621db4
Sahadat1057 <114007148+Sahadat1057@users.noreply.github.com>
2024-01-11 21:38:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let pages = []
-nextPage() {
 
+function nextPage() {
+    // Change the window location to page2.html
+    window.location.href = "page2.html";
 }
 
+// Add an event listener to the button
+document.getElementById("nextPage").addEventListener("click", nextPage);
