d216ae257335c018b9f8a61735af08d2f0d1e3ec
Sahadat1057 <114007148+Sahadat1057@users.noreply.github.com>
2024-01-18 10:26:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
-function nextPage() {
+function nextPage(num) {
     // Change the window location to page2.html
-    window.location.href = "page2.html";
+   if (num == 1) window.location.href = "page2.html";
+   if (num == 2 ) window.location.href = "page3.html";
+   if (num == 3 ) window.location.href = "page4.html";
 }
 
+
 document.getElementById("nextPage").addEventListener("click", nextPage);
+
+function previousPage(num) {
+    if (num == 1 ) window.location.href = "index.html";
+    if (num == 2) window.location.href = "page2.html"
+    if (num == 3) window.location.href = "page3.html"
+
+}
+
+document.getElementById("previousPage").addEventListener("click", previousPage);
+
